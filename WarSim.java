3ac308a41c71f0c3950169783aa8d94aaf934f23
Justin <justwells111@gmail.com>
2026-05-12 21:01:03 -0400

    lots of changes and improvements

--- WarSim.java
+++ WarSim.java
@@ -1,206 +1,160 @@
 package warsim;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+
+record WarResult(int winner, int turnCount) {};
 
 public class WarSim {
 
-    private static ArrayList<Integer> deck = new ArrayList<>();
-    
-    //this method adds cards with values 1-13 four times to the deck to simulate a real deck of cards. After, it shuffles the deck 4 times.
-    private static void InitializeDeck()
-    {
-        for(int i = 1; i < 14; i++)
-        {
-            deck.add(i);
-            deck.add(i);
-            deck.add(i);
-            deck.add(i);
-        }
-        Collections.shuffle(deck);
-        Collections.shuffle(deck);
-        Collections.shuffle(deck);
-        Collections.shuffle(deck);
-    }
-    
-    //this method adds x (times variable) amount of cards to wDeck from the top of lDeck. wDeck=winning/receiving deck, lDeck = losing/giving deck
-    private static void TransferCards(int times,ArrayList<Integer> wDeck,ArrayList<Integer> lDeck)
-    {
-        for (int i = 0; i < times; i++)
-        {
-            wDeck.add(lDeck.get(0));
-            lDeck.remove(0);
-        }   
-    }
-    
-    public static void main(String[] args) {
-        ArrayList<Integer> p1Deck = new ArrayList<>();
-        ArrayList<Integer> p2Deck = new ArrayList<>();
-        int winner=-1;
-        int choice=-1;
-        int tempChoice=-1;
-        int gameChoice = -1;
-        int turnCount = 0;
-        String line=" ";
-        String[] tempLine;
-        InitializeDeck();
+    private static final Random RANDOM = new Random();
+    private static final int MAXTURNS = 300;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Do you want to enter your cards, or run it with a random order of cards?\n1)My Own Cards\n2)Random Order\nInput: ");
-        gameChoice=scanner.nextInt();
-        scanner.nextLine();
-        
-        //this code block prompts the user to enter each players cards, with each card value being separated by a space. it then adds the entered values/cards into the
-        //corresponding player's deck
-        if(gameChoice==1)
-        {
-            System.out.print("Enter the first player's cards, with a space separating each. (Jack=11,Queen=12,King=13,Ace=1): ");
-            line=scanner.nextLine();
-            tempLine=line.split(" ");
-            for(int i=0;i<tempLine.length;i++)
-                p1Deck.add(Integer.parseInt(tempLine[i]));
-            System.out.print("Enter the second player's cards, with a space separating each. (Jack=11,Queen=12,King=13,Ace=1): ");
-            line=scanner.nextLine();
-            tempLine=line.split(" ");
-            for(int i=0;i<tempLine.length;i++)
-                p2Deck.add(Integer.parseInt(tempLine[i]));
-        }
-        
-        //this code block simulates if the cards were dealt out one at a time to each player.
-        else if (gameChoice==2)
-        {
-            for (int i = 0; i < deck.size(); i++) {
-                if (i % 2 == 0) {
-                    p1Deck.add(deck.get(i));
-                } else {
-                    p2Deck.add(deck.get(i));
-                }   
+    // this method adds cards with values 1-13 four times to the deck to simulate a real deck of cards.
+    // After, it shuffles the deck.
+    private static Deque<Integer> initializeDeck() {
+        List<Integer> deck = new ArrayList<>();
+        for (int i = 1; i < 14; i++) {
+            for (int suit = 0; suit < 4; suit++) {
+                deck.add(i);
             }
         }
-        else
-        {
-            System.out.println("Invalid Input. Try again");
+        Collections.shuffle(deck);
+        return new ArrayDeque<>(deck);
+    }
+
+    // this methods transfers the cards from the war pile to the winning deck
+    private static void transferCards(Deque<Integer> winningDeck, Deque<Integer> warPile) {
+        while (!warPile.isEmpty()) {
+            winningDeck.addLast(warPile.removeFirst());
         }
-        System.out.print("Do you want to see the cards after each bout?\n1)Yes\n2)No\n3)Surprise Me\nInput: ");
-        choice = scanner.nextInt();
-        if (choice == 3) {
-            tempChoice = (int) Math.ceil(Math.random() * 10);
-            if (tempChoice % 2 == 0) {
-                choice = 1;
-            } else {
-                choice = 2;
-            }
-        }
-        
-        //while both players have at least 1 card, and a winner hasn't been decided.
-        while (!p1Deck.isEmpty() && !p2Deck.isEmpty() && winner == -1) 
-        {
+    }
+
+    // this method gets a random boolean
+    private static int getSurpriseMeChoice() {
+        return RANDOM.nextBoolean() ? 1 : 2;
+    }
+
+    private static WarResult gameLoop(int choice, Deque<Integer> p1Deck, Deque<Integer> p2Deck) {
+        Deque<Integer> warPile = new ArrayDeque<>();
+        int winner = -1;
+        int turnCount = 0;
+        // while both players have at least 1 card, and a winner hasn't been decided.
+        while (!p1Deck.isEmpty() && !p2Deck.isEmpty() && winner == -1) {
             //if the user wants to see the cards after each bout/round. the if/else blocks adds a comma after each card if it's not the last one in the player's deck.
-            if (choice == 1) 
-            {
-                System.out.print("\nP1: ");
-                for (int i = 0; i < p1Deck.size(); i++) {
-                    if (i != p1Deck.size() - 1) {
-                        System.out.print(p1Deck.get(i) + ",");
-                    } else {
-                        System.out.print(p1Deck.get(i));
-                    }
-                }
-                System.out.print("\nP2: ");
-                for (int i = 0; i < p2Deck.size(); i++) {
-                    if (i != p2Deck.size() - 1) {
-                        System.out.print(p2Deck.get(i)+ ",");
-                    } else {
-                        System.out.print(p2Deck.get(i));
-                    }
-                }
-                System.out.println();
+            if (choice == 1) {
+                System.out.println(p1Deck);
+                System.out.println(p2Deck);
             }
             turnCount++;
-            if (p1Deck.get(0) < p2Deck.get(0)) {    //p1Deck lost
-                TransferCards(1,p2Deck,p1Deck);
-                TransferCards(1,p2Deck,p2Deck);
-            } else if (p1Deck.get(0) > p2Deck.get(0)) {     //p2Deck lost
-                TransferCards(1,p1Deck,p2Deck);
-                TransferCards(1,p1Deck,p1Deck);
-            } else if (p1Deck.get(0) == p2Deck.get(0)) {       //they equal once
-                if (p1Deck.size() < 5) {
+            if (turnCount == MAXTURNS) {
+                winner = 0;
+                break;
+            }
+            while (true) {
+                if (p1Deck.isEmpty()) {
                     winner = 2;
-                    p1Deck.clear();
                     break;
-                } else if (p2Deck.size() < 5) {
+                }
+                if (p2Deck.isEmpty()) {
                     winner = 1;
-                    p2Deck.clear();
                     break;
                 }
-                if (p1Deck.get(4) < p2Deck.get(4)) {     //equal once, p1Deck loses
-                    TransferCards(5,p2Deck,p1Deck);
-                    TransferCards(5,p2Deck,p2Deck);
-                } else if (p1Deck.get(4) > p2Deck.get(4)) {  //equal once, p1Deck wins
-                    TransferCards(5,p1Deck,p2Deck);
-                    TransferCards(5,p1Deck,p1Deck);
-                } else if (p1Deck.get(4) == p2Deck.get(4)) {   //equals twice
-                    if (p1Deck.size() < 9) {
+                int p1Card = p1Deck.removeFirst();
+                int p2Card = p2Deck.removeFirst();
+                warPile.addLast(p1Card);
+                warPile.addLast(p2Card);
+                if (p1Card < p2Card) {
+                    transferCards(p2Deck, warPile);
+                    break;
+                } else if (p1Card > p2Card) {
+                    transferCards(p1Deck, warPile);
+                    break;
+                }
+                for (int i = 0; i < 3; i++) {
+                    if (p1Deck.isEmpty()) {
                         winner = 2;
-                        p1Deck.clear();
                         break;
-                    } else if (p2Deck.size() < 9) {
+                    }
+                    if (p2Deck.isEmpty()) {
                         winner = 1;
-                        p2Deck.clear();
                         break;
                     }
-                    if (p1Deck.get(8) < p2Deck.get(8)) {  //equals twice, p1Deck loses
-                        TransferCards(9,p2Deck,p1Deck);
-                        TransferCards(9,p2Deck,p2Deck);
-                    } else if (p1Deck.get(8) > p2Deck.get(8)) {   //equals twice, p1Deck wins
-                        TransferCards(9,p1Deck,p2Deck);
-                        TransferCards(9,p1Deck,p1Deck);
-                    } else if (p1Deck.get(8) == p2Deck.get(8)) {  //equal three times
-                        if (p1Deck.size() < 13) {
-                            p1Deck.clear();
-                            winner = 2;
-                            break;
-                        } else if (p2Deck.size() < 13) {
-                            winner = 1;
-                            p2Deck.clear();
-                            break;
-                        }
-                        if (p1Deck.get(12) < p2Deck.get(12)) {   //equals three times, p1Deck loses
-                            TransferCards(13,p2Deck,p1Deck);
-                            TransferCards(13,p2Deck,p2Deck);
-                        } else if (p1Deck.get(12) > p2Deck.get(12)) {   //equals three times, p1Deck wins
-                            TransferCards(13,p1Deck,p2Deck);
-                            TransferCards(13,p1Deck,p1Deck);
-                        }
-                    }
+                    warPile.addLast(p1Deck.removeFirst());
+                    warPile.addLast(p2Deck.removeFirst());
+                }
+                if (winner != -1) {
+                    break;
                 }
             }
         }
-        
-        //output a message based on who won
-        if (winner == 1) {
-            System.out.println("P1 WON");
-        } else if (winner == 2) {
-            System.out.println("P2 WON");
-        } else if (p1Deck.size() != 0) {
-            System.out.println("P1 WON");
-        } else {
-            System.out.println("P2 WON");
-        }
+        return new WarResult(winner, turnCount);
+    }
 
-        System.out.print("Do you want to see how many turns it took?\n1)Yes\n2)No\n3)Surprise Me\nInput: ");
-        choice = scanner.nextInt();
-        if (choice == 3) {
-            tempChoice = (int) Math.ceil(Math.random() * 10);
-            if (tempChoice % 2 == 0) {
-                choice = 1;
-            } else {
-                choice = 0;
+    public static void main(String[] args) {
+        Deque<Integer> deck = initializeDeck();
+        Deque<Integer> p1Deck = new ArrayDeque<>();
+        Deque<Integer> p2Deck = new ArrayDeque<>();
+        int choice;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Do you want to enter your cards, or run it with a random order of cards?\n1)My Own Cards\n2)Random Order\nInput: ");
+            int gameChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            //this code block prompts the user to enter each players cards, with each card value being separated by a space. it then adds the entered values/cards into the
+            //corresponding player's deck
+            switch (gameChoice) {
+                case 1 -> {
+                    System.out.print("Enter the first player's cards, with a space separating each. (Jack=11,Queen=12,King=13,Ace=1): ");
+                    for (String line : scanner.nextLine().split(" ")) {
+                        p1Deck.add(Integer.parseInt(line));
+                    }
+                    System.out.print("Enter the second player's cards, with a space separating each. (Jack=11,Queen=12,King=13,Ace=1): ");
+                    for (String line : scanner.nextLine().split(" ")) {
+                        p2Deck.add(Integer.parseInt(line));
+                    }
+                }
+                case 2 -> {
+                    // this code block simulates if the cards were dealt out one at a time to each player.
+                    while (!deck.isEmpty()) {
+                        p1Deck.addLast(deck.removeFirst());
+                        p2Deck.addLast(deck.removeFirst());
+                    }
+                }
+                default ->
+                    System.out.println("Invalid Input. Try again");
             }
-        }
-        if (choice == 1) {
-            System.out.println("It took " + turnCount + " turns");
+            System.out.print("Do you want to see the cards after each bout?\n1)Yes\n2)No\n3)Surprise Me\nInput: ");
+            choice = scanner.nextInt();
+            if (choice == 3) {
+                choice = getSurpriseMeChoice();
+            }
+
+            WarResult result = gameLoop(choice, p1Deck, p2Deck);
+            //output a message based on who won
+            switch (result.winner()) {
+                case 0 ->
+                    System.out.println("Nobody WON... :(");
+                case 1 ->
+                    System.out.println("P1 WON");
+                case 2 ->
+                    System.out.println("P2 WON");
+            }
+
+            System.out.print("Do you want to see how many turns it took?\n1)Yes\n2)No\n3)Surprise Me\nInput: ");
+            choice = scanner.nextInt();
+            if (choice == 3) {
+                choice = getSurpriseMeChoice();
+            }
+            if (choice == 1) {
+                System.out.println("It took " + result.turnCount() + " turns");
+            }
         }
     }
 }
